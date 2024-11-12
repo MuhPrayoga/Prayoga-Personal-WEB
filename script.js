cc8d616077e928e2f579c1466546268f4ace87e7
Prayoga M <152146725+MuhPrayoga@users.noreply.github.com>
2024-11-12 21:00:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+AOS.init({
+    duration: 800,          // Durasi animasi dalam milidetik (0.8 detik)
+    easing: 'ease-in-out',  // Efek easing yang halus
+    once: false,            // Animasi diputar setiap kali elemen muncul
+    mirror: true,           // Memungkinkan animasi berfungsi saat scroll ke atas
+    offset: 100             // Mulai animasi saat elemen lebih dekat ke viewport
+});
